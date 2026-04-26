55d242b782af1c2e3e19a83cef9b60a96ddc03a7
Muhammad Ari Darmawan <aridarmawan716@gmail.com>
2026-04-26 09:52:08 +0700

    Add formatting preprocessor directives example

---
+++ Formatting Preprocessor Directive.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define VERY_LONG_CONSTANT \
+23.678901
+
+#define MAX 100
+#define MIN 0
+#    define SQUARE(x) \
+    x*x
+int main() {
+  printf("%d\n", VERY_LONG_CONSTANT * SQUARE(2));
+  return 0;
+}
